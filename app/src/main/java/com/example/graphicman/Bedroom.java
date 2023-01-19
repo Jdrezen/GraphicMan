90fea006d8bed844b540d31253d2f191cadad4e0
Dooglassed <clementregis1102@gmail.com>
2023-01-19 10:29:01 +0100

    first commit

---
+++ app/src/main/java/com/example/graphicman/Bedroom.java
@@ -0,0 +1,14 @@
+package com.example.graphicman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bedroom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bedroom);
+    }
+}
