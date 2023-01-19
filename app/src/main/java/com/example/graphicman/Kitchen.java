979f26128427969dd5adec6dea19286af74634c8
Jérémie Drezen <jeremie.drezen@gmail.com>
2023-01-19 10:58:03 +0100

    Navigation entre les activités

--- app/src/main/java/com/example/graphicman/Kitchen.java
+++ app/src/main/java/com/example/graphicman/Kitchen.java
@@ -2,7 +2,9 @@ package com.example.graphicman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Kitchen extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Kitchen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToGym(View view) {
+        Intent intent = new Intent(this, Gym.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToBedroom(View view) {
+        Intent intent = new Intent(this, Bedroom.class);
+        startActivity(intent);
+        finish();
     }
 }
