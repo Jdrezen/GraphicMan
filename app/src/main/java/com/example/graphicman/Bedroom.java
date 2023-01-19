382ee59085842e97babe58c84fa4e98c07d8b950
Dooglassed <clementregis1102@gmail.com>
2023-01-19 11:06:42 +0100

    fix button onclick and add sensor

--- app/src/main/java/com/example/graphicman/Bedroom.java
+++ app/src/main/java/com/example/graphicman/Bedroom.java
@@ -2,21 +2,57 @@ package com.example.graphicman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class Bedroom extends AppCompatActivity {
+public class Bedroom extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private TextView lightLevelText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bedroom);
+
+        lightLevelText = findViewById(R.id.light_level_text);
+        lightLevelText.setText("oui");
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float lightLevel = event.values[0];
+        lightLevelText.setText(String.valueOf(lightLevel));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used in this example
     }
 
     public void goToKitchen(View view) {
-        Intent intent = new Intent(this, Kitchen.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, Kitchen.class);
+//        startActivity(intent);
         finish();
     }
 }
