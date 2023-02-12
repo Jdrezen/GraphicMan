cef0c3a22361a38bac6f53fc970a43f27bd3e502
Jérémie Drezen <jeremie.drezen@gmail.com>
2023-02-12 15:57:17 +0100

    Ajustement pour que l'animation des tractions fonctionne sur tous les appareils

---
+++ app/src/main/java/com/example/graphicman/CanvasWrapper.java
@@ -0,0 +1,30 @@
+package com.example.graphicman;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class CanvasWrapper {
+
+    private Canvas canvas;
+    private float widthRatio;
+    private float heightRatio;
+    public CanvasWrapper(int width, int height){
+        this.widthRatio = width/1000f;
+        this.heightRatio = height/1000f;
+    }
+
+    public void setCanvas(Canvas canvas){
+        this.canvas = canvas;
+    }
+
+
+
+    public void drawRect(float left, float top, float right, float bottom, Paint paint){
+        canvas.drawRect(left*widthRatio,top*heightRatio,right*widthRatio,bottom*heightRatio,paint);
+    }
+
+    public void drawLine(float starX, float starY, float stopX, float stopY, Paint paint){
+        paint.setStrokeWidth(35*widthRatio);
+        canvas.drawLine(starX*widthRatio,starY*heightRatio,stopX*widthRatio,stopY*heightRatio,paint);
+    }
+}
